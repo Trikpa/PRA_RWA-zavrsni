72cfe0cc9bde80a65dc6545c101241871be29f12
Patrik Badanjak <pbadanj@racunarstvo.hr>
2020-06-04 22:27:22 +0200

    added model for "Djelatnik" and data repository class

---
+++ Site za administraciju/Models/Djelatnik.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Site_za_administraciju.Models
+{
+	public enum TipDjelatnika
+	{
+		StalniDjelatnik = 1,
+		HonorarniDjelatnik,
+		Student,
+		VoditeljTima,
+		Direktor,
+		Neaktivan
+	}
+	public class Djelatnik
+	{
+		public int IDDjelatnik { get; private set; }
+		public string Ime { get; set; }
+		public string Prezime { get; set; }
+		public TipDjelatnika Tip { get; set; }
+		public string Email { get; set; }
+		public string Lozinka { get; set; }
+		public DateTime DatumZaposlenja { get; set; }
+		public string Tim { get; set; }
+
+		public Djelatnik( int idDjelatnik, string ime, string prezime, int tip, string email, string lozinka, DateTime datumZaposlenja, string tim )
+		{
+			IDDjelatnik = idDjelatnik;
+			Ime = ime;
+			Prezime = prezime;
+			Tip = (TipDjelatnika)tip;
+			Email = email;
+			Lozinka = lozinka;
+			DatumZaposlenja = datumZaposlenja;
+			Tim = tim;
+		}
+
+		public override string ToString() => $"{Ime} {Prezime}";
+	}
+}
---
+++ Site za administraciju/Models/Repozitorij.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Microsoft.ApplicationBlocks.Data;
+using Utilities;
+
+namespace Site_za_administraciju.Models
+{
+	public static class Repozitorij
+	{
+		public static DataSet Ds { get; set; }
+		private static readonly string cs = ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+
+		public static Djelatnik LoginUser( string email, string password )
+		{
+			SqlParameter[] parameters = new SqlParameter[4];
+
+			parameters[0] = new SqlParameter("@email", SqlDbType.NVarChar, 50)
+			{
+				Direction = ParameterDirection.Input,
+				Value = email
+			};
+
+			parameters[1] = new SqlParameter("@password", SqlDbType.NVarChar, 50)
+			{
+				Direction = ParameterDirection.Input,
+				Value = password
+			};
+
+			parameters[2] = new SqlParameter("@djelatnikID", SqlDbType.Int)
+			{
+				Direction = ParameterDirection.Output
+			};
+
+			Ds = SqlHelper.ExecuteDataset(cs, CommandType.StoredProcedure, "ProvjeriKorisnickoImeIZaporku", parameters);
+
+			int idDjelatnik = (int)parameters[2].Value;
+
+			if ( idDjelatnik > 0 )
+				return GetDjelatnik(idDjelatnik);
+
+			return null;
+		}
+
+		public static Djelatnik GetDjelatnik( int idDjelatnik )
+		{
+			DataTable tblDjelatnik = SqlHelper.ExecuteDataset(cs, "DohvatiPodatkeODjelatniku", idDjelatnik).Tables[0];
+
+			if ( tblDjelatnik.Rows.Count == 0 ) return null;
+
+			DataRow row = tblDjelatnik.Rows[0];
+			return new Djelatnik
+			(
+				idDjelatnik: (int)row["IDDjelatnik"],
+				ime: row["Ime"].ToString(),
+				prezime: row["Prezime"].ToString(),
+				tip: (int)row["TipID"],
+				email: row["Email"].ToString(),
+				lozinka: row["Lozinka"].ToString(),
+				datumZaposlenja: DateTime.Parse(row["DatumZaposlenja"].ToString()),
+				tim: row["Tim"].ToString()
+			);
+		}
+	}
+}
