18818ffb862b9b62401f3ae8a5c448a3c163c4cf
Patrik Badanjak <pbadanj@racunarstvo.hr>
2020-06-07 23:19:07 +0200

    Added team details view

---
+++ Site za administraciju/DetaljiTima.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Site_za_administraciju.Models;
+
+namespace Site_za_administraciju
+{
+	public partial class DetaljiTima : System.Web.UI.Page
+	{
+		private Tim t;
+		protected void Page_Load( object sender, EventArgs e )
+		{
+			if ( Request.QueryString["id"] == null || Session["djelatnik"] == null )
+				Response.Redirect("Timovi.aspx");
+			else
+			{
+				int timID = int.Parse(Request.QueryString["id"]);
+				t = Repozitorij.GetTim(timID);
+				FillForm();
+			}
+		}
+
+		private void FillForm()
+		{
+			tbNaziv.Text = t.Naziv;
+			tbVoditelj.Text = t.Voditelj.ToString();
+		}
+
+		protected void BtnSpremi_Click( object sender, EventArgs e ) => UpdateProjekt();
+
+		private void UpdateProjekt()
+		{
+			t.Naziv = tbNaziv.Text;
+
+			if ( Repozitorij.UpdateTim(t) )
+				btnPovratak.ForeColor = Color.Red;
+			else
+				btnPovratak.ForeColor = Color.White;
+
+			ChangeTextBoxFieldsState(enabled: false);
+		}
+
+		protected void BtnPovratak_Click( object sender, EventArgs e ) => Response.Redirect("Timovi.aspx");
+		protected void BtnUredi_Click( object sender, EventArgs e ) => ChangeTextBoxFieldsState(enabled: true);
+
+		private void ChangeTextBoxFieldsState( bool enabled ) => tbNaziv.Enabled = enabled;
+	}
+}
---
+++ Site za administraciju/User_Controls/TimUserControl.ascx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Site_za_administraciju.Models;
+
+namespace Site_za_administraciju.User_Controls
+{
+	public partial class TimUserControl : System.Web.UI.UserControl
+	{
+		protected void Page_Load( object sender, EventArgs e )
+		{
+
+		}
+
+		public void SetInfo( Tim t )
+		{
+			lblTeamName.Text = t.Naziv;
+			BtnDetalji.Attributes.Add("id", t.IDTim.ToString());
+		}
+
+		protected void OpenTimInfoPage( LinkButton b )
+			=> Response.Redirect($"DetaljiTima.aspx?id={int.Parse(b.Attributes["id"])}");
+
+		protected void BtnDetalji_Click( object sender, EventArgs e )
+		{
+			LinkButton b = sender as LinkButton;
+			OpenTimInfoPage(b);
+		}
+	}
+}
