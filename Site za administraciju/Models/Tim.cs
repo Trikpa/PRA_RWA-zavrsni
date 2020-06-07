71a08d230e53b9578e8ea0309a697bd961ce5222
Patrik Badanjak <pbadanj@racunarstvo.hr>
2020-06-07 23:19:33 +0200

    Tim model

---
+++ Site za administraciju/Models/Tim.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Site_za_administraciju.Models
+{
+	public class Tim
+	{
+		public int IDTim { get; set; }
+		public string Naziv { get; set; }
+		public Djelatnik Voditelj { get; set; }
+
+		public Tim( int idTim, string naziv, Djelatnik voditelj )
+		{
+			IDTim = idTim;
+			Naziv = naziv;
+			Voditelj = voditelj;
+		}
+
+		public override string ToString() => Naziv;
+	}
+}
