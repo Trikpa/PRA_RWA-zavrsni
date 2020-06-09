cabc5a8359c2a1944aef08dd9beccfc1965172f0
Patrik Badanjak <pbadanj@racunarstvo.hr>
2020-06-10 00:19:41 +0200

    Added Djelatnici page

---
+++ Site za administraciju/Djelatnici.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/DashboardMasterPage.Master" AutoEventWireup="true" CodeBehind="Djelatnici.aspx.cs" Inherits="Site_za_administraciju.Djelatnici" %>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="dashboardTitle" runat="server">
+	<h3 class="text-white mb-0">Djelatnici</h3>
+</asp:Content>
+
+<asp:Content ID="Content3" ContentPlaceHolderID="body" runat="server">
+	<div class="row">
+		<div class="col-md-6" runat="server" id="projectContainer">
+			<asp:PlaceHolder ID="phDjelatnici" runat="server"></asp:PlaceHolder>
+		</div>
+		<div class="col-md-6">
+			<asp:LinkButton ID="btnDodaj" runat="server" CssClass="btn btn-info" OnClick="BtnDodaj_Click"><i class="fas fa-plus">&nbsp;</i>Dodaj</asp:LinkButton>
+		</div>
+	</div>
+</asp:Content>
---
+++ Site za administraciju/Djelatnici.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Site_za_administraciju.Models;
+
+namespace Site_za_administraciju
+{
+	public partial class Djelatnici : System.Web.UI.Page
+	{
+		protected void Page_Load( object sender, EventArgs e )
+		{
+			if ( Session["djelatnik"] == null )
+				Response.Redirect("Login.aspx");
+
+			ShowAllDjelatnici();
+		}
+
+		private void ShowAllDjelatnici()
+		{
+			IEnumerable<Djelatnik> djelatnici = Repozitorij.GetDjelatniciWithoutDirectors();
+			foreach ( Djelatnik djelatnik in djelatnici )
+			{
+				DjelatnikUserControl puc = LoadControl("User_Controls/DjelatnikUserControl.ascx") as DjelatnikUserControl;
+				puc.ID = $"{djelatnik.IDDjelatnik}";
+				puc.SetInfo(djelatnik);
+
+				phDjelatnici.Controls.Add(puc);
+			}
+		}
+
+		protected void BtnDodaj_Click( object sender, EventArgs e ) => Response.Redirect("DodajDjelatnika.aspx");
+	}
+}
