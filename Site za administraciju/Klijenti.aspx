131fee1f5c1548f116c6de40af8fedd411d370a8
Patrik Badanjak <pbadanj@racunarstvo.hr>
2020-06-10 22:50:56 +0200

    Added page for viewing klijenti

---
+++ Site za administraciju/Klijenti.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/DashboardMasterPage.Master" AutoEventWireup="true" CodeBehind="Klijenti.aspx.cs" Inherits="Site_za_administraciju.Klijenti" %>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="dashboardTitle" runat="server">
+	<h3 class="text-white mb-0">Klijenti</h3>
+</asp:Content>
+
+<asp:Content ID="Content3" ContentPlaceHolderID="body" runat="server">
+	<div class="row">
+		<div class="col-md-6" runat="server" id="projectContainer">
+			<asp:PlaceHolder ID="phKlijenti" runat="server"></asp:PlaceHolder>
+		</div>
+		<div class="col-md-6">
+			<asp:LinkButton ID="btnDodaj" runat="server" CssClass="btn btn-info" OnClick="BtnDodaj_Click"><i class="fas fa-plus">&nbsp;</i>Dodaj</asp:LinkButton>
+		</div>
+	</div>
+</asp:Content>
---
+++ Site za administraciju/Klijenti.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Site_za_administraciju.Models;
+using Site_za_administraciju.User_Controls;
+
+namespace Site_za_administraciju
+{
+	public partial class Klijenti : System.Web.UI.Page
+	{
+		protected void Page_Load( object sender, EventArgs e )
+		{
+			if ( Session["djelatnik"] == null )
+				Response.Redirect("Login.aspx");
+
+			ShowAllKlijenti();
+		}
+
+		private void ShowAllKlijenti()
+		{
+			IEnumerable<Klijent> klijenti = Repozitorij.GetKlijenti();
+			foreach ( Klijent klijent in klijenti )
+			{
+				KlijentUserControl puc = LoadControl("User_Controls/KlijentUserControl.ascx") as KlijentUserControl;
+				puc.ID = $"{klijent.IDKlijent}";
+				puc.SetInfo(klijent);
+
+				phKlijenti.Controls.Add(puc);
+			}
+		}
+
+		protected void BtnDodaj_Click( object sender, EventArgs e ) => Response.Redirect("DodajKlijenta.aspx");
+	}
+}
