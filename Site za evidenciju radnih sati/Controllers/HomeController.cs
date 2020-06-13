921cb49e09c9ad6077e0811564ebd32ae75e5bda
Patrik Badanjak <pbadanj@racunarstvo.hr>
2020-06-13 12:42:16 +0200

    Created layout and index page

---
+++ Site za evidenciju radnih sati/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Site_za_evidenciju_radnih_sati.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		[Route("pregled_rada")]
+		public ActionResult PregledRada()
+		{
+			return View();
+		}
+
+		[Route("potvrda_satnica")]
+		public ActionResult PotvrdaSatnica()
+		{
+			return View();
+		}
+	}
+}
