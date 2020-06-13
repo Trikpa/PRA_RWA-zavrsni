e6d3d9623b624edad824f564cd8a778a3717ce8d
Patrik Badanjak <pbadanj@racunarstvo.hr>
2020-06-13 13:45:57 +0200

    Proprietary controllers

--- Site za evidenciju radnih sati/Controllers/PregledRadaController.cs
+++ Site za evidenciju radnih sati/Controllers/PregledRadaController.cs
@@ -6,11 +6,11 @@ using System.Web.Mvc;
 
 namespace Site_za_evidenciju_radnih_sati.Controllers
 {
-	public class HomeController : Controller
-	{
-		public ActionResult Index()
-		{
-			return View();
-		}
-	}
+    public class PregledRadaController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
 }
